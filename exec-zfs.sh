5030c211c153136a80b3b5b04a56aff895585f7a
arader <ardr@outlook.com>
2016-07-21 17:06:24 -0700

    Add pool support for exec-zfs
    This commit starts teaching exec-zfs how to parse `zpool` output. It
    also makes it so the script is testable against a collection of known
    outputs.
    
    You can override the commands used to gather ZFS data, allowing
    you to inject your own data to be parsed. For example, to force
    exec-zfs.sh to parse the output of zpool on a mirrored pool:
    
    ```
    $ LIST_DATASETS_CMD=" " \
        LIST_POOLS_CMD="echo test" \
        POOL_STATUS_CMD="cat tests/mirror-10-minute-scrub.status" \
        exec-zfs.sh
    ```
    This will override the script so that no datasets are parsed, a
    fake 'test' pool has its status read, and that the pool status to
    be parsed is the output contained in the
    `tests/mirror-10-minute-scrub.stats` file will be used instead.
    
     Please enter the commit message for your changes. Lines starting

--- exec-zfs.sh
+++ exec-zfs.sh
@@ -3,25 +3,132 @@
 # A collectd 'exec' script used to get zfs dataset statistics
 
 HOSTNAME="${COLLECTD_HOSTNAME:-$(hostname -f)}"
-INTERVAL="${COLLECTD_INTERVAL:-10}"
+INTERVAL="${COLLECTD_INTERVAL:-300}"
 
-print_dataset()
+LIST_POOLS_CMD="${LIST_POOLS_CMD:-zpool list -H -o name}"
+POOL_STATUS_CMD="${POOL_STATUS_CMD:-zpool status \$1}"
+
+LIST_DATASETS_CMD="${LIST_DATASETS_CMD:-zfs list -Hp -o name,usedds,usedchild,usedsnap,usedrefreserv,avail}" 
+
+if [ "$INTERVAL" -lt 300 ]
+then
+    INTERVAL=300
+fi
+
+arg()
 {
-    dataset=$(echo $2 | sed 's|/|.|g')
-
-    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedds interval=$INTERVAL $1:$3"
-    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedchild interval=$INTERVAL $1:$4"
-    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedsnap interval=$INTERVAL $1:$5"
-    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedrefreserv interval=$INTERVAL $1:$6"
-    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-avail interval=$INTERVAL $1:$7"
+    shift $1
+    echo $2
 }
 
-while sleep "$INTERVAL"
-do
-    time="$(date +%s)"
-
-    zfs list -Hp -o name,usedds,usedchild,usedsnap,usedrefreserv,avail | while read line
+process_pools()
+{
+    $LIST_POOLS_CMD | while read line
     do
-        print_dataset $time $line
+        process_pool $line
     done
+}
+
+process_pool()
+{
+    local parsing='scan'
+
+    eval $POOL_STATUS_CMD | while read line
+    do
+        case $parsing in
+            scan)
+                echo $line | grep -E '^scan: ' > /dev/null 2>&1
+        
+                if [ $? == 0 ]
+                then
+                    process_pool_scrub_status $1 "$line"
+
+                    parsing='status_header'
+                fi
+                ;;
+            status_header)
+                echo $line | grep -E '^NAME[	 ]+STATE[	 ]+READ[	 ]+WRITE[	 ]+CKSUM$' > /dev/null 2>&1
+
+                if [ $? == 0 ]
+                then
+                    parsing='status_info'
+                fi
+                ;;
+            status_info)
+                if [ "$line" == "" ]
+                then
+                    parsing=done
+                    break
+                fi
+
+                process_pool_member_status $1 "$line"
+
+                ;;
+            *)
+                ;;
+        esac
+    done
+}
+
+process_pool_scrub_status()
+{
+    #TODO handle running scrub
+
+    local repaired="U"
+    local duration="U"
+    local errors="U"
+
+    if [ "$2" != "scan: none requested" ]
+    then
+        local scrub_status=$(echo "$2" | sed -E 's/[^0-9]+ ([0-9]+) in ([0-9]+h[0-9]+m) with ([0-9]+) errors on (.*)/\1 \2 \3 \4/')
+        repaired=$(arg 0 $scrub_status)
+        duration=$(arg 1 $scrub_status | sed -E 's/^([0-9]+)h([0-9]+)m$/\1*3600 + \2*60/' | bc)
+        errors=$(arg 2 $scrub_status)
+    fi
+
+    echo "PUTVAL $HOSTNAME/zpool-$1/count-scrub-repaired interval=$INTERVAL $TIME:$repaired"
+    echo "PUTVAL $HOSTNAME/zpool-$1/duration-scrub interval=$INTERVAL $TIME:$duration"
+    echo "PUTVAL $HOSTNAME/zpool-$1/count-scrub-errors interval=$INTERVAL $TIME:$errors"
+}
+
+process_pool_member_status()
+{
+    local device=$(arg 0 $2 | sed -e 's|^/||' -e 's|/|.|')
+    local read_count=$(arg 2 $2)
+    local write_count=$(arg 3 $2)
+    local cksum_count=$(arg 4 $2)
+
+    echo "PUTVAL $HOSTNAME/zpool-$1/count-read-errors.$device interval=$INTERVAL $TIME:$read_count"
+    echo "PUTVAL $HOSTNAME/zpool-$1/count-write-errors.$device interval=$INTERVAL $TIME:$write_count"
+    echo "PUTVAL $HOSTNAME/zpool-$1/count-cksum-errors.$device interval=$INTERVAL $TIME:$cksum_count"
+}
+
+process_datasets()
+{
+    $LIST_DATASETS_CMD | while read line
+    do
+        process_dataset $line
+    done
+}
+
+process_dataset()
+{
+    dataset=$(echo $1 | sed 's|/|.|g')
+
+    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedds interval=$INTERVAL $TIME:$2"
+    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedchild interval=$INTERVAL $TIME:$3"
+    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedsnap interval=$INTERVAL $TIME:$4"
+    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-usedrefreserv interval=$INTERVAL $TIME:$5"
+    echo "PUTVAL $HOSTNAME/zfs-$dataset/bytes-avail interval=$INTERVAL $TIME:$6"
+}
+
+while true
+do
+    TIME="$(date +%s)"
+
+    process_pools
+
+    process_datasets
+
+    sleep "$INTERVAL"
 done
